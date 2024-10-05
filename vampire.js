ed763abbb6cac3a3f1065f8e3b2500619e5d6773
amusictheorist <a_millaresthomson@outlook.com>
2024-10-05 10:46:31 -0400

    Finish methods

--- vampire.js
+++ vampire.js
@@ -39,15 +39,69 @@ class Vampire {
     return false;
   }
 
-  /** Stretch **/
-
   // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampire1 = this;
+    const lineage1 = [];
+    while (currentVampire1) {
+      lineage1.push(currentVampire1);
+      currentVampire1 = currentVampire1.creator;
+    }
+    let currentVampire2 = vampire;
+    const lineage2 = [];
+    while (currentVampire2) {
+      lineage2.push(currentVampire2);
+      currentVampire2 = currentVampire2.creator;
+    }
 
+    lineage1.reverse();
+    lineage2.reverse();
+
+    const minLength = Math.min(lineage1.length, lineage2.length);
+    let commonAncestor = null;
+    for (let i = 0; i < minLength; i++) {
+      if (lineage1[i] === lineage2[i]) {
+        commonAncestor = lineage1[i]
+      } else {
+        break;
+      }
+    }
+    return commonAncestor;
+  }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+    if (this.name === name) {
+      return this;
+    }
+    for (const offspring of this.offspring) {
+      const foundVampire = offspring.vampireWithName(name);
+      if (foundVampire) {
+        return foundVampire;
+      }
+    }
+    return null;
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+    let totalDescendents = 0;
+    for (const offspring of this.offspring) {
+        totalDescendents += 1 + offspring.totalDescendents;
+    }
+    return totalDescendents;
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
+    let millennialVampires = [];
+    if (this.yearConverted > 1980) {
+      millennialVampires.push(this);
+    }
+    for (const offspring of this.offspring) {
+      millennialVampires = millennialVampires.concat(offspring.allMillennialVampires);
+    }
+    return millennialVampires;
   }
 }
 
